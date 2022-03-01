4327fdec186bc0ffff9bf0698748aa8dcf88aae1
parvez13913 <parvezz13913@gmail.com>
2022-03-02 00:39:13 +0600

    vanish button complite

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 const lightOnOfButton = on => {
     const lightOf = document.getElementById('bulb-off');
     const lightOn = document.getElementById("bulb-on");
+    const vanishButton = document.getElementById("vanish-button");
     if(on === true){
         lightOn.style.display = "block";
         lightOf.style.display = "none";
+        vanishButton.disabled = true;
     }
     else{
         lightOf.style.display = "block";
         lightOn.style.display = "none";
+        vanishButton.disabled = true;
     }
+}
+const vanishButton = () => {
+    const onButton = document.getElementById("on-button");
+    const ofButton = document.getElementById("of-button");
+    onButton.disabled = true;
+    ofButton.disabled = true;
 }
